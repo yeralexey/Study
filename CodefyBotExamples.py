9d88358c04a11ba743a95fc6b46a5e8305e7d8bd
loki <yer.alexey@gmail.com>
2023-02-16 15:54:22 +0300

    Coderwars solution

--- CodefyBotExamples.py
+++ CodefyBotExamples.py
@@ -54,3 +54,47 @@ def get_city_name():
     return cities
 
 print(get_city_name())
+
+
+#checks if Tic-Tac_Toe solved.
+# Receive board 3x3 array, value is 0 if empty, 1 if it is "X", or 2 if it is an "O" .
+# Return -1 if board not finished AND no one has won, 1 if "X" won, 2 if "O" won, 0 if it's a cat's. Name is_solved
+
+
+def is_solved(board):
+    """
+    https://t.me/CodefyChannel/45
+    https://www.codewars.com/kata/525caa5c1bf619d28c000335/ - 5 kye
+
+    Function checks if Tic-Tac_Toe solved.
+    :param board: 3x3 array, value is 0 if empty, 1 if it is "X", or 2 if it is an "O"
+    :return: -1 if board not finished AND no one has won, 1 if "X" won, 2 if "O" won, 0 if it's a cat's
+
+    by @CodefyBot, t.me
+    """
+    # Checking rows
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != 0:
+            return board[i][0]
+    # Checking columns
+    for i in range(3):
+        if board[0][i] == board[1][i] == board[2][i] != 0:
+            return board[0][i]
+    # Checking diagonals
+    if board[0][0] == board[1][1] == board[2][2] != 0:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != 0:
+        return board[0][2]
+    # Checking if board is full
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                return -1
+    return 0
+
+
+# How to call example:
+board = [[1, 2, 1],
+         [2, 1, 2],
+         [2, 1, 2]]
+print(is_solved(board))
